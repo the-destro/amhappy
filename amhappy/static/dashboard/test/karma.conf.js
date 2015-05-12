df521cb3619a9d7ce7c7ab9d069c75247b75e190
the_destro <francis.ridder@gmail.com>
2015-05-12 13:50:53 -0700

    rebuild

--- amhappy/static/dashboard/test/karma.conf.js
+++ amhappy/static/dashboard/test/karma.conf.js
@@ -26,6 +26,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-strap/dist/angular-strap.js',
+      'bower_components/angular-strap/dist/angular-strap.tpl.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
